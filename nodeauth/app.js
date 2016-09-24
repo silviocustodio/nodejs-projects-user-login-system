8ec11c1f4ad80ed87b31ebd5b9ada335585223c0
Silvio Custodio <adm.ti.silviocustodio@gmail.com>
2016-09-23 21:57:49 -0300

    NodeAuth -  fix register - flash problem

--- nodeauth/app.js
+++ nodeauth/app.js
@@ -6,11 +6,12 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var passport = require('passport');
-var expressValidator = require('express-validator');
 var LocalStrategy = require('passport-local').Strategy;
+var expressValidator = require('express-validator');
 var multer = require('multer');
-var upload = multer({dest: './uploads'})
+var upload = multer({dest: './uploads'});
 var flash = require('connect-flash');
+var bcrypt = require('bcryptjs');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 var db = mongoose.connection;
@@ -24,18 +25,14 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 // Handle Sessions
-app.set(session({
+app.use(session({
   secret:'secret',
   saveUninitialized: true,
   resave: true
@@ -63,7 +60,10 @@ app.use(expressValidator({
   }
 }));
 
-app.use(require('connect-flash')());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(flash());
 app.use(function (req, res, next) {
   res.locals.messages = require('express-messages')(req, res);
   next();
